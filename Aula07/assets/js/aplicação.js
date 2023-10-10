20225e2e1cb983b4f2dfd50b040fa0d85309b356
FilipeHanniel <filipehanniel@gmail.com>
2023-10-10 19:07:28 -0300

    Usando a API para carregar a página

--- Aula07/assets/js/aplicação.js
+++ Aula07/assets/js/aplicação.js
@@ -1,48 +1,48 @@
-const produtos = [
-    {
-        tipo: "capacete",
-        nome: "Capacete 1",
-        alt: "Isto é um capacete, modelo1",
-        preco: 50,
-        imageURL: 'images/capacete01.png'
-    },
-    {   
-        tipo: "capacete",
-        nome: "Capacete 2",
-        alt: "Isto é um capacete, modelo2",
-        preco: 60,
-        imageURL: 'images/capacete02.png'
-    },
-    {   
-        tipo: "capacete",
-        nome: "Capacete 3",
-        alt: "Isto é uma blusa, modelo3",
-        preco: 130,
-        imageURL: 'images/capacete03.png',
-    },
-    {
-        tipo: "blusão",
-        nome: "Blusão 1",
-        alt: "Isto é uma blusa, modelo1",
-        preco: 100,
-        imageURL: 'images/blusa01.png'
-    },
-    {   
-        tipo: "blusão",
-        nome: "Blusão 2",
-        alt: "Isto é um capacete, modelo2",
-        preco: 150,
-        imageURL: 'images/blusa02.png'
-    },
-    {   
-        tipo: "blusão",
-        nome: "Blusão 3",
-        alt: "Isto é uma blusa, modelo3",
-        preco: 99,
-        imageURL: 'images/blusa03.png'
-    }
+// const produtos = [
+//     {
+//         "tipo": "capacete",
+//         "nome": "Capacete 1",
+//         "alt": "Isto é um capacete, modelo1",
+//         "preco": 50,
+//         "imageURL": "images/capacete01.png"
+//     },
+//     {   
+//         "tipo": "capacete",
+//         "nome": "Capacete 2",
+//         "alt": "Isto é um capacete, modelo2",
+//         "preco": 60,
+//         "imageURL": "images/capacete02.png"
+//     },
+//     {   
+//         "tipo": "capacete",
+//         "nome": "Capacete 3",
+//         "alt": "Isto é um capacete, modelo3",
+//         "preco": 130,
+//         "imageURL": "images/capacete03.png"
+//     },
+//     {
+//         "tipo": "blusão",
+//         "nome": "Blusão 1",
+//         "alt": "Isto é uma blusa, modelo1",
+//         "preco": 100,
+//         "imageURL": "images/blusa01.png"
+//     },
+//     {   
+//         "tipo": "blusão",
+//         "nome": "Blusão 2",
+//         "alt": "Isto é uma blusa, modelo2",
+//         "preco": 150,
+//         "imageURL": "images/blusa02.png"
+//     },
+//     {   
+//         "tipo": "blusão",
+//         "nome": "Blusão 3",
+//         "alt": "Isto é uma blusa, modelo3",
+//         "preco": 99,
+//         "imageURL": "images/blusa03.png"
+//     }
     
-]
+// ]
 
 const carrinho = [];
 
@@ -64,8 +64,11 @@ const criarProduto = (nome, alt, preco,imageURL, elemento, index) => {
     "<h2>"+ nome + "</h2>    <p>R$ " + preco + "</p> <button class='adicionarCarrinho' onclick='adicionarCarrinho("+ index +")'>Adicionar ao carrinho</button> </div>";
 }
 
-const buscarProdutos = () => {
-    const lista = produtos;
+const buscarProdutos = async () => {
+    
+    var resp = await fetch('https://run.mocky.io/v3/3a8883b0-775e-4668-b270-8b2d3e301718');
+        
+    const lista = await resp.json();
     lista.forEach((item, index) => {
         let elemento = ""
         if (item.tipo === 'capacete') {
@@ -79,6 +82,6 @@ const buscarProdutos = () => {
     
 }
 
-mostrarQuantidadeItensCarrinho();
+// mostrarQuantidadeItensCarrinho();
 buscarProdutos();
 
